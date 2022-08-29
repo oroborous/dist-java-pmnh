0224d60c3c3232f40be37181503d12643f863ad0
Stacy Read <sread@wctc.edu>
2022-08-29 18:09:35 -0500

    Added internationalization option

--- src/main/java/edu/wctc/pmnh/PmnhApplication.java
+++ src/main/java/edu/wctc/pmnh/PmnhApplication.java
@@ -2,12 +2,39 @@ package edu.wctc.pmnh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
-public class PmnhApplication {
+public class PmnhApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(PmnhApplication.class, args);
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 
 }
