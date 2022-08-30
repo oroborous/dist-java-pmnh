8f72d2c13d3b7621786ba7ce51305f28b4967073
Stacy Read <sread@wctc.edu>
2022-08-30 09:48:37 -0500

    Added comments to bootstrap class customizations

--- src/main/java/edu/wctc/pmnh/PmnhApplication.java
+++ src/main/java/edu/wctc/pmnh/PmnhApplication.java
@@ -13,6 +13,10 @@ import java.util.Locale;
 
 @SpringBootApplication
 public class PmnhApplication implements WebMvcConfigurer {
+    // WebMvcConfigurer interface contains callback methods that
+    // can be overridden to customize the default behavior of
+    // Spring MVC
+    // https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/servlet/config/annotation/WebMvcConfigurer.html
 
     public static void main(String[] args) {
         SpringApplication.run(PmnhApplication.class, args);
@@ -21,12 +25,18 @@ public class PmnhApplication implements WebMvcConfigurer {
     @Bean
     public LocaleResolver localeResolver() {
         SessionLocaleResolver slr = new SessionLocaleResolver();
+        // Defaults requests to originating from the US
+        // https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/servlet/i18n/SessionLocaleResolver.html
+        // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Accept-Language
         slr.setDefaultLocale(Locale.US);
         return slr;
     }
 
     @Bean
     public LocaleChangeInterceptor localeChangeInterceptor() {
+        // Allows for changing the locale with every request by
+        // reading the defined request parameter
+        // https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/servlet/i18n/LocaleChangeInterceptor.html
         LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
         lci.setParamName("lang");
         return lci;
@@ -34,6 +44,7 @@ public class PmnhApplication implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        // Create an interceptor bean and put it into use
         registry.addInterceptor(localeChangeInterceptor());
     }
 
